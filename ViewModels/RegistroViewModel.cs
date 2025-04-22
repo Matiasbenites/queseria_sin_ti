ee9c5e770fa511f86028eee04c739c173eecf470
Javier Frias <jabfryyy@gmail.com>
2025-04-21 23:05:42 -0300

    Estilos siguiendo la base del modelo aplicados.

--- ViewModels/RegistroViewModel.cs
+++ ViewModels/RegistroViewModel.cs
@@ -4,27 +4,27 @@ namespace QueseriaSoftware.ViewModels
 {
     public class RegistroViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Formato de email incorrecto")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
         [Display(Name = "Nombre de Usuario")]
         public string? NombreDeUsuario { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La contraseña es obligatorio.")]
         [DataType(DataType.Password)]
         [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
         public string? Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El DNI es obligatorio.")]
         [Display(Name = "DNI")]
         public string? Dni { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El Nombre es obligatorio.")]
         public string? Nombre { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
         public string? Apellido { get; set; }
 
         [Phone]
