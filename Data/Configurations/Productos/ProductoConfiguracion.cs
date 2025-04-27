32b7ee86996eacd302a0616f8ef209fcb6fd08d1
Javier Frias <jabfryyy@gmail.com>
2025-04-26 22:18:13 -0300

    Insert de mas ejemplos, imagen de producto y modificacion de posicion del boton volver a inicio en catalogo

--- Data/Configurations/Productos/ProductoConfiguracion.cs
+++ Data/Configurations/Productos/ProductoConfiguracion.cs
@@ -10,20 +10,46 @@ namespace QueseriaSoftware.Data.Configurations.Productos
         {
 
             builder.HasData(
-            new Producto
-            {
-                Id = 1,
-                Nombre = "Queso Manchego",
-                Descripcion = "Queso tradicional español de leche de oveja",
-                Precio = 15.99m,
-                Stock = 100,
-                ImgUrl = "/images/queso-manchego.jpg",
-                IdCategoria = 1,
-                CreadoEn = new DateTime(2024, 4, 26, 0, 0, 0, DateTimeKind.Utc),
-                IdMarca = 1,
-                Activo = true,
-            }
-        );
+                new Producto
+                {
+                    Id = 1,
+                    Nombre = "Queso Manchego",
+                    Descripcion = "Queso tradicional español de leche de oveja",
+                    Precio = 15.99m,
+                    Stock = 100,
+                    ImgUrl = "/images/queso-manchego.jpg",
+                    IdCategoria = 1,
+                    CreadoEn = new DateTime(2024, 4, 26, 0, 0, 0, DateTimeKind.Utc),
+                    IdMarca = 1,
+                    Activo = true,
+                },
+                new Producto
+                {
+                    Id = 2,
+                    Nombre = "Queso Gouda",
+                    Descripcion = "Queso holandés suave y cremoso",
+                    Precio = 12.50m,
+                    Stock = 0,
+                    ImgUrl = "/images/queso-gouda.jpg",
+                    IdCategoria = 1,
+                    CreadoEn = new DateTime(2024, 4, 26, 0, 0, 0, DateTimeKind.Utc),
+                    IdMarca = 1,
+                    Activo = true,
+                },
+                new Producto
+                {
+                    Id = 3,
+                    Nombre = "Queso Parmesano",
+                    Descripcion = "Queso italiano duro de leche de vaca, perfecto para pastas",
+                    Precio = 18.75m,
+                    Stock = 10,
+                    ImgUrl = "/images/queso-parmesano.jpg",
+                    IdCategoria = 1,
+                    CreadoEn = new DateTime(2024, 4, 26, 0, 0, 0, DateTimeKind.Utc),
+                    IdMarca = 1,
+                    Activo = true,
+                }
+            );
         }
     }
 }
---
+++ Migrations/20250427010230_InsertProductosEjemplo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QueseriaSoftware.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertProductosEjemplo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "Id", "Activo", "CreadoEn", "Descripcion", "IdCategoria", "IdMarca", "ImgUrl", "ModificadoEn", "Nombre", "Precio", "Stock" },
+                values: new object[,]
+                {
+                    { 2, true, new DateTime(2024, 4, 26, 0, 0, 0, 0, DateTimeKind.Utc), "Queso holandés suave y cremoso", 1, 1, "/images/queso-gouda.jpg", null, "Queso Gouda", 12.50m, 0 },
+                    { 3, true, new DateTime(2024, 4, 26, 0, 0, 0, 0, DateTimeKind.Utc), "Queso italiano duro de leche de vaca, perfecto para pastas", 1, 1, "/images/queso-parmesano.jpg", null, "Queso Parmesano", 18.75m, 10 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
