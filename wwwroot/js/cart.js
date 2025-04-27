66a34eb50683edaf30f30dd1b01959fbf7f93d1d
Javier Frias <jabfryyy@gmail.com>
2025-04-26 23:17:16 -0300

    script de validacion

---
+++ wwwroot/js/cart.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    // Validación para cada formulario de agregar al pedido
+    $('.form-agregar-al-pedido').on('submit', function (e) {
+        var cantidadInput = $(this).find('input[name="Cantidad"]');
+        var cantidad = parseInt(cantidadInput.val(), 10);
+
+        if (isNaN(cantidad) || cantidad < 1) {
+            e.preventDefault(); // Evita el envío del formulario
+            alert('Por favor ingrese una cantidad válida mayor o igual a 1.');
+            cantidadInput.focus();
+        }
+    });
+});
