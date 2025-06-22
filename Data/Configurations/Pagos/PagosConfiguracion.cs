7e7dfebc4330d61e9ad2c92d411ab89668f40a19
JavierF <jabfryyy@gmail.com>
2025-06-22 04:10:08 -0300

    Se insertaron medios de pago iniciales

---
+++ Data/Configurations/Pagos/PagosConfiguracion.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QueseriaSoftware.Models;
+
+namespace QueseriaSoftware.Data.Configurations.Pagos
+{
+    public class PagosConfiguracion : IEntityTypeConfiguration<Pago>
+    {
+        public void Configure(EntityTypeBuilder<Pago> builder)
+        {
+            builder.HasData(
+                 new Pago
+                 {
+                     Id = 1,
+                     Nombre = "Transferencia",
+                     Activo = true
+                 },
+                 new Pago
+                 {
+                     Id = 2,
+                     Nombre = "Efectivo",
+                     Activo = true
+                 },
+                 new Pago
+                 {
+                     Id = 3,
+                     Nombre = "Tarjeta",
+                     Activo = false
+                 }
+             );
+        }
+    }
+}
---
+++ Migrations/20250622070923_InsertarRegistrosDePagos.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QueseriaSoftware.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertarRegistrosDePagos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Pagos",
+                columns: new[] { "Id", "Activo", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, true, "Transferencia" },
+                    { 2, true, "Efectivo" },
+                    { 3, false, "Tarjeta" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pagos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Pagos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Pagos",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
